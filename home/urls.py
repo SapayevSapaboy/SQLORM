774def5d724f3aef8bd6e8bc0c669c63f4214b7f
Sapoboy <sopoyev1991@gmail.com>
2023-11-09 21:36:37 +0300

    table <th> qushildi

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', homeview, name='home'),
     path('oquv_yili/', oquv_yiliview, name='oquv_yili'),
+    path('fanlarga_birikish/', fanlarga_birikishview, name='fanlarga_birikishview'),
    # path('fanlar/<str:kafedra_id>/',fanlarview, name='fanlarview'),
     path('yonalishlar/<str:kafedra_id>/',yonalish_view, name='yonalishlarview'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from django.db.models import F
 from django.db.models import Q
 from .models import *
 
+
 # Create your views here.
 
 def homeview(request):
@@ -28,9 +29,7 @@ def yonalish_view(request, kafedra_id):
     return render(request, "fanlar.html", context)
 
 
-
 def fanlarga_birikishview(request):
-
     #
     # context = {
     #     "year_list1": EEducationYear.objects.all().order_by('code'),
@@ -43,11 +42,13 @@ def fanlarga_birikishview(request):
 
     queryset = (
         HSemestr.objects
-        .select_related('field_education_year', 'field_curriculum__field_department')
-        .filter(field_curriculum__field_education_form='11', field_curriculum__field_education_year='2023')
+        .select_related('field_education_year', 'field_curriculum__field_department','field_curriculum_name')
+        .filter(field_curriculum__field_education_form='11', field_curriculum__field_education_year='2023',field_curriculum__field_department='2')
         .order_by('field_curriculum__field_education_year')
-        .values('field_curriculum__field_department__name','field_curriculum__field_education_year', 'field_curriculum__field_education_year__name','field_education_year','name')
-        .distinct()
+        .values('field_curriculum__field_department__name',
+                'field_curriculum__field_education_year', 'field_curriculum__field_education_year__name',
+                'field_education_year', 'name','field_curriculum__name')
+        .distinct()[:100]
     )
 
     context = {
@@ -55,10 +56,6 @@ def fanlarga_birikishview(request):
     }
 
     return render(request, "fanlarga_birikish.html", context)
-
-
-
-
 
 
 def oquv_yiliview(request):
@@ -70,15 +67,15 @@ def oquv_yiliview(request):
     #     cur_ids.append(i.field_curriculum_id)
     #
 
-
-    student_list = EStudentMeta.objects.filter(field_student_status=11, active=True,field_curriculum__in=cur_ids).values('field_department__name',
-                                                                                            'field_education_year__name').annotate(
+    student_list = EStudentMeta.objects.filter(field_student_status=11, active=True,
+                                               field_curriculum__in=cur_ids).values('field_department__name',
+                                                                                    'field_education_year__name').annotate(
         count=Count('field_department__name')).order_by('field_department__name', 'field_education_year__name').filter(
-        ~Q(field_department_id__in=[7, 8, 77,6]))
+        ~Q(field_department_id__in=[7, 8, 77, 6]))
 
     year_list = EStudentMeta.objects.filter(field_student_status=11, active=True).values(
         'field_education_year__name').annotate(count=Count('id')).filter(
-        ~Q(field_department_id__in=[7, 8, 77,6]))
+        ~Q(field_department_id__in=[7, 8, 77, 6]))
     sum_year_list = 0
     for i in year_list:
         sum_year_list += i['count']
